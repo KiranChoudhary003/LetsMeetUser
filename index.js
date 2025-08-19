ffa6ab942d49d98480ce756e558f71ae90977345
Hemant Paliwal <hemantpaliwal@Hemants-MacBook-Air.local>
2025-08-19 17:51:27 +0530

    Build Success

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-// import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
 import App from './App';
+import { name as appName } from './app.json';
+
+console.log('index.js loaded'); // Metro should log this
 
 AppRegistry.registerComponent(appName, () => App);
